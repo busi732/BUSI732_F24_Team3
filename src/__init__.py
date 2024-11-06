28e8d1fa0ca1c31303005ed01c23663f8a2ae444
laalsanedo <rpatel1320@yahoo.com>
2024-11-05 20:36:01 -0500

    added the eda file

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+from read_data import process_raw_data
+from eda import create_eda_report
+
+create_eda_report()
---
+++ src/eda.py
@@ -0,0 +1,13 @@
+from read_data import process_raw_data
+from dataprep.eda import create_report
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+# # Load the data
+# processed_df = process_raw_data()
+
+# # Generate an EDA report
+# eda_report = create_report(processed_df)
+
+# # Saving it to the 
+# eda_report.save("../reports/eda_report.html")
